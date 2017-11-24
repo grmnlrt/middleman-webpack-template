ca729b08dd3227f42041ac19b4e04b624955c307
germainloret <germain.loret@gmail.com>
2017-11-24 15:46:34 +0100

    add webpack

--- config.rb
+++ config.rb
@@ -5,27 +5,19 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+activate :external_pipeline,
+    name: :webpack,
+    command: build? ?
+    "./node_modules/webpack/bin/webpack.js --bail -p" :
+    "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+    source: "build",
+    latency: 1
 
+# Layouts
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
 
 # Helpers
 # Methods defined in the helpers block are available in templates
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,2 @@
-// This is where it all goes :)
+import { loadDynamicBannerText } from './components/test';
+loadDynamicBannerText();
