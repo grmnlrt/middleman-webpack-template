061d258ede34dc617efb7483632fd57c1a4abcbe
germainloret <germain.loret@gmail.com>
2017-11-27 10:44:24 +0100

    middleman webpack template ready !

--- config.rb
+++ config.rb
@@ -33,25 +33,10 @@ page '/*.txt', layout: false
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
 configure :build do
-  # "Ignore" JS and CSS so webpack has full control.
-  # ignore { |path| path =~ /\/(.*)\.js|css$/ && $1 != "all" && $1 != "vendor" }
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Enable cache buster
-  # activate :asset_hash
-
   # Use relative URLs
   activate :relative_assets
-
   config[:relative_links] = true
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
 
 activate :deploy do |deploy|
